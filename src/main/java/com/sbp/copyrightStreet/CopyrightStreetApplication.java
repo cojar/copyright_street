b482043a78be9c9ea64588e93abf1e74c5e636c4
sik2 <devpang20@gmail.com>
2023-07-24 10:41:14 +0900

    init

---
+++ src/main/java/com/sbp/copyrightStreet/CopyrightStreetApplication.java
@@ -0,0 +1,13 @@
+package com.sbp.copyrightStreet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopyrightStreetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopyrightStreetApplication.class, args);
+	}
+
+}
