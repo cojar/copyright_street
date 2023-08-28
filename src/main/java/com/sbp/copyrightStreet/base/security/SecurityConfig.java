6ba1868b601c1ab733dd336af0485d510766864a
sik2 <devpang20@gmail.com>
2023-08-28 22:27:04 +0900

    prod

--- src/main/java/com/sbp/copyrightStreet/base/security/SecurityConfig.java
+++ src/main/java/com/sbp/copyrightStreet/base/security/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -39,6 +38,7 @@ public class SecurityConfig {
                 .csrf().ignoringRequestMatchers(
                         new AntPathRequestMatcher("/**"))
                 .and()
+                .cors().disable()
                 .formLogin()
                 .loginPage("/member/login")
                 .defaultSuccessUrl("/")
