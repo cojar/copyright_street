f0dfa0066c2ab11ae3ebad885baa56a5ec2d13a6
sik2 <devpang20@gmail.com>
2023-08-29 05:02:44 +0900

    cors

--- src/main/java/com/sbp/copyrightStreet/base/WebMvcConfig.java
+++ src/main/java/com/sbp/copyrightStreet/base/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.sbp.copyrightStreet.base;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +16,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/gen/**")
                 .addResourceLocations("file:///" + genFileDirPath + "/");
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
